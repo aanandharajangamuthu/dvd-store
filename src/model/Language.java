eeae3dbbd8dd85f45d55b1c478d3a0dbd79522ea
aanandharajangamuthu <anandharaj@ideas2it.com>
2016-07-22 15:58:23 +0530

    Language class modified

--- src/model/Language.java
+++ src/model/Language.java
@@ -3,9 +3,6 @@
  */
 package model;
 
-import java.sql.Date;
-import java.util.Set;
-import java.util.HashSet;
 
 /**
  * @author Anandharaj Angamuthu
@@ -13,13 +10,8 @@ import java.util.HashSet;
  */
 public class Language {
 	
-	private int id;
+    private int id;
     private String name;
-    private Set<Disc> discs = new HashSet<Disc>; 
-    private String createdBy;
-    private Date createdAt;
-    private String modifiedBy;
-    private Date modifiedAt;
     
 
     public Language(){}
@@ -29,8 +21,8 @@ public class Language {
 	this.name = name;
     }
     
-//Getter and Setter
-
+  //Getter and Setter
+    
 	public int getId() {
 		return id;
 	}
@@ -47,50 +39,9 @@ public class Language {
 		this.name = name;
 	}
 
-	public Set<Disc> getDiscs() {
-		return discs;
-	}
-
-	public void setDiscs(Set<Disc> discs) {
-		this.discs = discs;
-	}
-
-	public String getCreatedBy() {
-		return createdBy;
-	}
-
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-
-	public Time getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Time createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
-
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-
-	public Time getModifiedAt() {
-		return modifiedAt;
-	}
-
-	public void setModifiedAt(Time modifiedAt) {
-		this.modifiedAt = modifiedAt;
-	}
-
 	@Override
 	public String toString() {
-		return "Language [id=" + id + ", name=" + name + ", createdBy=" + createdBy + ", createdAt=" + createdAt
-				+ ", modifiedBy=" + modifiedBy + ", modifiedAt=" + modifiedAt + "]";
+		return "Language [id=" + id + ", name=" + name + "]";
 	}
 
 }
